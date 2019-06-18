c046874c44b3c15ed79838110f02730c5a86b5a4
Subhodip <subho3010@gmail.com>
2019-06-18 19:07:53 +0530

    Update firebase.js
    removed firebase credentials

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -1,13 +1,13 @@
 import * as firebase from 'firebase'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDHzZB_HLY6ZiV-14G4iIz1U2kb3JC9PDo",
-    authDomain: "login2-4a4fd.firebaseapp.com",
-    databaseURL: "https://login2-4a4fd.firebaseio.com",
-    projectId: "login2-4a4fd",
-    storageBucket: "login2-4a4fd.appspot.com",
-    messagingSenderId: "190833033056",
-    appId: "1:190833033056:web:33dae81cf9dd9281"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   };
 
   class Firebase {
